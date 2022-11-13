55faca7d89178701335f0d1c6d245aef8f7209f8
spalen0 <spalen@proton.me>
2022-11-13 20:56:34 +0100

    test: right balance

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,6 +59,7 @@ def create_vault(project, gov):
         asset,
         governance=gov,
         deposit_limit=MAX_INT,
+        max_profit_locking_time=WEEK,
     ):
         vault = gov.deploy(
             project.dependencies["yearn-vaults"]["master"].VaultV3,
@@ -66,6 +67,7 @@ def create_vault(project, gov):
             "VaultV3",
             "AV",
             governance,
+            max_profit_locking_time,
         )
         # set vault deposit
         vault.set_deposit_limit(deposit_limit, sender=gov)
