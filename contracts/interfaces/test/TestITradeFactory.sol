feae7a70c13738e36a797aab150a098d69b1ac2f
spalen0 <116267321+spalen0@users.noreply.github.com>
2022-11-25 18:03:16 +0000

    feat: add ySwap (#2)

---
+++ contracts/interfaces/test/TestITradeFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.14;
+
+interface TestITradeFactory {
+    struct AsyncTradeExecutionDetails {
+        address _strategy;
+        address _tokenIn;
+        address _tokenOut;
+        uint256 _amount;
+        uint256 _minAmountOut;
+    }
+
+    function enable(address _tokenIn, address _tokenOut) external;
+
+    function execute(
+        AsyncTradeExecutionDetails calldata _tradeExecutionDetails,
+        address _swapper,
+        bytes calldata _data
+    ) external returns (uint256 _receivedAmount);
+
+    function grantRole(bytes32 role, address account) external;
+
+    function STRATEGY() external view returns (bytes32);
+}
